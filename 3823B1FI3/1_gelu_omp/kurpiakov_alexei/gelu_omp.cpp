2bd474e06940b5846da2262bf5bf145f8ed89db2
Aleksei Kurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2026-04-17 19:43:44 +0300

    Update gelu_omp.cpp

--- 3823B1FI3/1_gelu_omp/kurpiakov_alexei/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/kurpiakov_alexei/gelu_omp.cpp
@@ -19,8 +19,8 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
 #pragma omp parallel for simd schedule(static)
     for (int i = 0; i < (int)n; ++i) {
         float x = input[i];
-        float x2 = x * x ;
-        float x3 = x * x2
+        float x2 = x * x;
+        float x3 = x * x2;
         float arg = C1 * (x + C2 * x3);
 
         output[i] = x / (1.0f + std::exp(-arg)); 
